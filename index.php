1f0eb0de2f418dc1119516f3861e03bed8fbca79
Alindra Putra <alindrapu@gmail.com>
2023-08-13 08:12:10 +0700

    add: gifts section, audio play, unscrollable on load, footer section

--- index.php
+++ index.php
@@ -20,14 +20,22 @@
   <link rel="stylesheet" href="countdown/simplyCountdown.theme.default.css" />
   <script src="countdown/simplyCountdown.min.js"></script>
   <script src="/index.js"></script>
-
-</head>
-
-<body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
     crossorigin="anonymous"></script>
 
+  <script>
+    function enableScroll() {
+      document.body.classList.remove('unscrollable');
+
+      var audio = new Audio('/sound/sound.mp3');
+      audio.play();
+    }
+  </script>
+</head>
+
+
+<body class="unscrollable">
   <section id="hero"
     class="hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white">
     <main>
@@ -44,33 +52,13 @@
       }
       ?>
       <div id="#namaundangan"></div>
-      <a href="#home" class="btn btn-lg mt-4">Lihat Undangan</a>
+      <a href="#home" class="btn btn-lg mt-4" onclick="enableScroll()">Lihat Undangan</a>
     </main>
   </section>
 
   <nav class="navbar navbar-expand-md sticky-top mynavbar">
     <div class="container">
       <a class="navbar-brand" href="#">Peti & Syukron</a>
-      <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-        aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">P & S
-          </h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <div class="navbar-nav ms-auto">
-            <a class="nav-link" href="#home">Home</a>
-            <a class="nav-link" href="#info">Info</a>
-            <a class="nav-link" href="#gallery">Gallery</a>
-            <a class="nav-link" href="#rsvp">RSVP</a>
-            <a class="nav-link" href="#gifts">Gifts</a>
-          </div>
-        </div>
-      </div>
     </div>
   </nav>
 
@@ -224,7 +212,189 @@
     </div>
   </section>
 
+  <section id="gifts" class="gifts">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8 col-10 text-center">
+          <span>DOA RESTU SERTA</span>
+          <h2>Hadiah Pernikahan</h2>
 
+          <!-- <div class="row d-flex justify-content-center">
+            <div class="col-md-8 col-lg-6">
+              <div class="card shadow-0 border" style="background-color: #f0f2f5;">
+                <div class="comment card-body p-4">
+                  <div class="form-outline mb-4">
+                    <input type="text" id="addAName" class="form-control mb-2" placeholder="Nama" />
+                    <input type="text" id="addANote" class="form-control" placeholder="Doa & Ucapan Selamat" />
+                    <button id="submitComment" type="button" class="mt-2">Kirim Ucapan</button>
+                  </div>
+
+                  <div class="comment card mb-4">
+                    <div class="card-body">
+                      <p>Type your note, and hit enter to add it</p>
+
+                      <div class="d-flex justify-content-between">
+                        <div class="d-flex flex-row align-items-center">
+                          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(4).webp" alt="avatar" width="25"
+                            height="25" />
+                          <p class="small mb-0 ms-2">Martha</p>
+                        </div>
+                        <div class="d-flex flex-row align-items-center">
+                          <p class="small text-muted mb-0">Upvote?</p>
+                          <i class="far fa-thumbs-up mx-2 fa-xs text-black" style="margin-top: -0.16rem;"></i>
+                          <p class="small text-muted mb-0">3</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card mb-4">
+                    <div class="card-body">
+                      <p>Type your note, and hit enter to add it</p>
+
+                      <div class="d-flex justify-content-between">
+                        <div class="d-flex flex-row align-items-center">
+                          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar" width="25"
+                            height="25" />
+                          <p class="small mb-0 ms-2">Johny</p>
+                        </div>
+                        <div class="d-flex flex-row align-items-center">
+                          <p class="small text-muted mb-0">Upvote?</p>
+                          <i class="far fa-thumbs-up mx-2 fa-xs text-black" style="margin-top: -0.16rem;"></i>
+                          <p class="small text-muted mb-0">4</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card mb-4">
+                    <div class="card-body">
+                      <p>Type your note, and hit enter to add it</p>
+
+                      <div class="d-flex justify-content-between">
+                        <div class="d-flex flex-row align-items-center">
+                          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp" alt="avatar" width="25"
+                            height="25" />
+                          <p class="small mb-0 ms-2">Mary Kate</p>
+                        </div>
+                        <div class="d-flex flex-row align-items-center text-primary">
+                          <p class="small mb-0">Upvoted</p>
+                          <i class="fas fa-thumbs-up mx-2 fa-xs" style="margin-top: -0.16rem;"></i>
+                          <p class="small mb-0">2</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card">
+                    <div class="card-body">
+                      <p>Type your note, and hit enter to add it</p>
+
+                      <div class="d-flex justify-content-between">
+                        <div class="d-flex flex-row align-items-center">
+                          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar" width="25"
+                            height="25" />
+                          <p class="small mb-0 ms-2">Johny</p>
+                        </div>
+                        <div class="d-flex flex-row align-items-center">
+                          <p class="small text-muted mb-0">Upvote?</p>
+                          <i class="far fa-thumbs-up ms-2 fa-xs text-black" style="margin-top: -0.16rem;"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> -->
+
+
+
+          <div class="card">
+            <div class="card-body">
+              <blockquote class="blockquote mb-1">
+                <p>"<i>Dan di antara tanda-tanda kekuasaan-Nya diciptakan-Nya untukmu pasangan hidup dari jenismu
+                    sendiri supaya kamu dapat ketenangan hati dan dijadikannya kasih sayang di antara kamu. <br>
+                    Sesungguhnya yang demikian menjadi tanda-tanda kebesaran-Nya bagi orang-orang yang berpikir.</i>"
+                </p>
+                <footer class="blockquote-footer"> Q.S. AR-RUM:21
+                </footer>
+              </blockquote>
+            </div>
+          </div>
+          <h4>Doa restu anda merupakan karunia yang sangat berarti bagi kami dan jika memberi adalah ungkapan tanda
+            kasih anda, maka anda dapat memberi kado secara cashless.</h4>
+        </div>
+        <div class="row justify-content-center mt-2">
+          <div class="col-md-5 col-10">
+            <div class="card text-center text-bg-light mb-5">
+              <div class="card-header">
+                rekening
+              </div>
+              <div class="card-body">
+                <div class="row justify-content-center">
+                  <div class="col-md-12">
+                    <i class="bi bi-bank d-block"></i>
+                    <h3 class="mt-5 mb-5">Bank Central Asia (BCA) <br> A.N Syukron Al Azhar <br>6790284150 </h3>
+                    <a href="#gifts" onclick="copyToClipboard('6790284150')"
+                      class="btn btn-lg mt-0 mb-2 bg-light-subtle">Salin No. Rekening</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center mt-4">
+          <div class="col-md-5 col-10">
+            <div class="card text-center text-bg-light ">
+              <div class="card-header">
+                rekening
+              </div>
+              <div class="card-body">
+                <div class="row justify-content-center">
+                  <div class="col-md-10">
+                    <i class="bi bi-bank d-block"></i>
+                    <h3 class="mt-5 mb-5">Bank Central Asia (BCA) <br> A.N Peti Ningsih <br>8880447107 </h3>
+                    <a href="#gifts" onclick="copyToClipboard('8880447107')"
+                      class="btn btn-lg mt-0 mb-2 bg-light-subtle">Salin No. Rekening</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
+
+  <section id="close" class="close">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8 col-10 text-center pb-5">
+          <h2>Peti & Syukron</h2>
+          <p>Atas kehadiran dan doarestunya kami ucapkan terima kasih.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <footer id="footer" class="bg-dark pt-5 justify-content-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <p class="text-center">Crafted by : Alindra Putra</p>
+        <div class="col-1 align-items-center">
+          <h3><a href="https://instagram.com/semuaindera" target="_blank"><i class="bi bi-instagram"></i></a></h3>
+        </div>
+        <div class="col-1 justify-content-center">
+          <h3><a href="https://wa.me/6281289389920" target="_blank"><i class="bi bi-whatsapp"></i></a></h3>
+        </div>
+        <p class="text-light text-center">Copyright &copy; 2023 All Rights Reserved.</p>
+      </div>
+    </div>
+  </footer>
 
 
 
@@ -275,6 +445,9 @@
     import {
       initializeApp
     } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+    import {
+      getFirestore, getDoc, Doc
+    } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore-compat.js";
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -290,21 +463,20 @@
 
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
+    const db = getFirestoreDatabase(app);
   </script>
   <!-- End of Firebase Script -->
 
-
   <script>
-    const stickyTop = document.querySelector('.sticky-top');
-    const hamburger = document.querySelector('.navbar-toggler');
-    const buttonClose = document.querySelector('.btn-close');
-
-    hamburger.addEventListener('click', function () {
-      stickyTop.style.overflow = 'visible';
-    });
-    buttonClose.addEventListener('click', function () {
-      stickyTop.style.overflow = 'hidden';
-    });
+    function copyToClipboard(text) {
+      var dummyInput = document.createElement('textarea');
+      document.body.appendChild(dummyInput);
+      dummyInput.value = text;
+      dummyInput.select();
+      document.execCommand('copy');
+      document.body.removeChild(dummyInput);
+      alert("No. Rekening berhasil disalin: " + text);
+    }
   </script>
 
   <!-- <script>
